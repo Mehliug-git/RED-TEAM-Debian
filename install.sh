5b61220d2d7f6bed245420f876f2161e37ab9952
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2024-06-24 11:42:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Deb
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
 sudo apt update -y
 sudo apt install fish -y
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bobthefish
 
 sudo apt install build-essential zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ruby ruby-dev
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
