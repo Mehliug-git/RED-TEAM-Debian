6d96a3b0e04891a0b328267d3e9b64a4a7ecb8be
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2024-06-24 11:39:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-sudo su
-sudo apt install python3 python3-pip perl git wgel curl  gnupg2
-sudo apt install default-jdk
-sudo apt install openjdk-17-jdk
-sudo apt install nmap
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+sudo apt install python3 python3-pip perl git wget curl gnupg2 -y
+sudo apt install default-jdk -y 
+sudo apt install openjdk-17-jdk -y
+sudo apt install nmap -y
 
 git clone https://github.com/aircrack-ng/aircrack-ng
 cd aircrack-ng
@@ -14,8 +14,8 @@ ldconfig
 
 echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
 curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
-sudo apt update
-sudo apt install fish
+sudo apt update -y
+sudo apt install fish -y
 
 sudo apt install build-essential zlib1g zlib1g-dev libpq-dev libpcap-dev libsqlite3-dev ruby ruby-dev
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
